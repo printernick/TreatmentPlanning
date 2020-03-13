2584956634e8ed4a08201a58137fe4796ae2aa8f
Nicholas Tran <phikhant@uci.edu>
2020-03-13 19:54:27 -0400

    Changed debug back. Removed link to admin page

--- TreatmentPlanning/settings.py
+++ TreatmentPlanning/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['dare2treat.herokuapp.com' , '127.0.0.1']
 
