7cc28eb1ed96a5fe277a2e925cd30fc709463502
Nicholas Tran <phikhant@uci.edu>
2020-03-13 20:08:01 -0400

    Some change

--- TreatmentPlanning/settings.py
+++ TreatmentPlanning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+#DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['dare2treat.herokuapp.com' , '127.0.0.1']
